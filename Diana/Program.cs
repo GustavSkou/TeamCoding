72d1389cc08fc862d011e1bde1cdd6ad471cfabd
dichi24 <diana.chirica222@gmail.com>
2024-09-24 14:17:36 +0200

    Add files via upload

--- Diana/Program.cs
+++ Diana/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Tal()
+{
+	Random random = new Random();
+	
+		int a = random.Next(1,101);
+		return a;
+}
+
+
+void over50(int b)
+{
+	if ( b > 50)
+	{
+		Console.WriteLine("Tallet er over 50");
+		
+	}
+	else if (b < 50)
+	{
+		Console.WriteLine("Tallet er under 50");
+	}
+	else 
+	{
+		Console.WriteLine("Tallet er 50");
+	}
+	if ( b % 2 == 0) 
+	{
+		Console.WriteLine("Lige tal");
+	}
+	else 
+	{
+		Console.WriteLine("Ikke lige tal");
+	}
+}
+for (int i = 0; i < 10; i++)
+	{ 	
+		int tal = Tal();
+		Console.WriteLine(tal);
+		over50(tal);
+	}
